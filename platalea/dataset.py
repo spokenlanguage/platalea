abd5968b2bf184ba634e813010879cf06cbbc3c1
Grzegorz Chrupała <g.chrupala@uvt.nl>
2019-09-13 13:54:35 +0200

    Loading val/test data in platalea.dataset

--- platalea/dataset.py
+++ platalea/dataset.py
@@ -7,7 +7,7 @@ class Flickr8KData(torch.utils.data.Dataset):
     def __init__(self, root, split='train'):
         self.root = root
         self.split = split
-
+        self.metadata = json.load(open(root + 'dataset.json'))['images']
         # mapping from image id to list of caption id
         self.image_captions = {}
         for line in open(root + 'wav2capt.txt'):
@@ -17,7 +17,7 @@ class Flickr8KData(torch.utils.data.Dataset):
 
         # image, caption pairs
         self.split_data = []
-        for image in json.load(open(root + 'dataset.json'))['images']:
+        for image in self.metadata:
             if image['split'] == self.split:
                 for text_id, audio_id in self.image_captions[image['filename']]:
                     self.split_data.append((image['filename'], audio_id, image['sentences'][text_id]['raw']))
@@ -42,32 +42,57 @@ class Flickr8KData(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.split_data)
 
-
-def collate_fn(data, max_frames=2048):
-    #data.sort(key=lambda x: len(x[1]), reverse=True)
-    images, texts, audios = zip(* [ (datum['image'], datum['text'], datum['audio']) for datum in data ])
-
-    # Merge images (from tuple of 3D tensor to 4D tensor).
-    images = torch.stack(images, 0)
-
-    # Merge captions
-    # truncate to max_frames
-    # pad with zeros
+    def evaluation(self):
+        """Returns image features, caption features, and a boolean array specifying whether a caption goes with an image."""
+        audio = []
+        text  = []
+        image = []
+        matches = []
+        for img in self.metadata:
+            if img['split'] == self.split:
+                image.append(self.image[img['filename']])
+                for text_id, audio_id in self.image_captions[img['filename']]:
+                    text.append(img['sentences'][text_id])
+                    audio.append(self.audio[audio_id])
+                    matches.append((len(audio)-1, len(image)-1))
+        correct = torch.zeros(len(audio), len(image)).bool()
+        for i,j in matches:
+            correct[i,j] = True
+        return dict(image=image, audio=audio, text=text, correct=correct)
+    
+def batch_audio(audios, max_frames=20148):
+    """Merge audio captions. Truncate to max_frames. Pad with zeros."""
     mfcc_lengths = [len(cap[:max_frames,:]) for cap in audios]
     mfcc = torch.zeros(len(audios), max(mfcc_lengths), audios[0].size(1)) 
     for i, cap in enumerate(audios):
         end = mfcc_lengths[i]
         mfcc[i, :end] = cap[:end]
-    mfcc = mfcc.permute(0, 2, 1)
-    # Merge captions (from tuple of 1D tensor to 2D tensor).
-    # pad with zeros
+    return mfcc.permute(0, 2, 1), torch.tensor(mfcc_lengths)
+
+def batch_text(texts):
+    """Merge captions (from tuple of 1D tensor to 2D tensor). Pad with zeros."""
     # FIXME this needs to be done properly, eventually
     char_lengths = [len(cap) for cap in texts]
     chars = torch.zeros(len(texts), max(char_lengths)).long()
     for i, cap in enumerate(texts):
         end = char_lengths[i]
         chars[i, :end] = cap[:end]        
-    return dict(image=images, audio=mfcc, text=chars, audio_len=torch.tensor(mfcc_lengths), text_len=torch.tensor(char_lengths))
+    return chars, torch.tensor(char_lengths)
+
+def batch_image(images):
+    return torch.stack(images, 0)
+
+def collate_fn(data, max_frames=2048):
+    #data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, texts, audios = zip(* [ (datum['image'], datum['text'], datum['audio']) for datum in data ])
+
+    # Merge images (from tuple of 3D tensor to 4D tensor).
+    images = batch_images(images, 0)
+
+    mfcc, mfcc_lengths = batch_audio(audios, max_frames=max_frames)
+    chars, char_lengths = batch_text(texts)
+    
+    return dict(image=images, audio=mfcc, text=chars, audio_len=mfcc_lengths, text_len=char_lengths)
 
 def flickr8k_loader(split='train', batch_size=32, shuffle=False, max_frames=2048):
     return torch.utils.data.DataLoader(dataset=Flickr8KData(root='/roaming/gchrupal/datasets/flickr8k/', split=split), 
@@ -75,3 +100,5 @@ def flickr8k_loader(split='train', batch_size=32, shuffle=False, max_frames=2048
                                        shuffle=shuffle,
                                        num_workers=0,
                                        collate_fn=lambda x: collate_fn(x, max_frames=max_frames))
+
+        
