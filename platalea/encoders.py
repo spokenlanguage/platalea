1c7549bdd8293774a25176892baca5b33647f768
Lieke Gelderloos <u1270964@green.uvt.nl>
2019-10-16 14:27:18 +0200

    inout instead of self.inout

--- platalea/encoders.py
+++ platalea/encoders.py
@@ -13,7 +13,7 @@ class ImageEncoder(nn.Module):
         self.norm = config['norm']
         self.linear_transform = nn.Linear(in_features = linear['in_size'], out_features = linear['out_size'])
         nn.init.xavier_uniform_(self.linear_transform.weight.data)
-        
+
     def forward(self, input):
         x = self.linear_transform(input)
         if self.norm:
@@ -26,23 +26,23 @@ class SpeechEncoder(nn.Module):
         super(SpeechEncoder, self).__init__()
         conv = config['conv']
         rnn  = config['rnn']
-        att  = config ['att'] 
+        att  = config ['att']
         self.Conv = nn.Conv1d(**conv)
         self.RNN = nn.GRU(batch_first=True, **rnn)
         self.att = Attention(**att)
-        
+
     def forward(self, input, l):
         x = self.Conv(input)
         # update the lengths to compensate for the convolution subsampling
         # l = [int((y-(self.Conv.kernel_size[0]-self.Conv.stride[0]))/self.Conv.stride[0]) for y in l]
-        l = self.inout(self.Conv, l)
+        l = inout(self.Conv, l)
         # create a packed_sequence object. The padding will be excluded from the update step
         # thereby training on the original sequence length only
         x = nn.utils.rnn.pack_padded_sequence(x.transpose(2,1), l, batch_first=True, enforce_sorted=False)
         x, hx = self.RNN(x)
         # unpack again as at the moment only rnn layers except packed_sequence objects
         x, lens = nn.utils.rnn.pad_packed_sequence(x, batch_first = True)
-        x = nn.functional.normalize(self.att(x), p=2, dim=1)    
+        x = nn.functional.normalize(self.att(x), p=2, dim=1)
         return x
 
     def introspect(self, input, l):
@@ -55,20 +55,20 @@ class SpeechEncoder(nn.Module):
         conv = self.Conv(input).permute(0, 2, 1)
         l = inout(self.Conv, l)
         result['conv'] = [ conv[i, :l[i], :] for i in range(len(conv)) ]
-        
+
         logging.info("Computing full stack of RNN states")
         conv_padded = nn.utils.rnn.pack_padded_sequence(conv, l, batch_first=True, enforce_sorted=False)
         rnn  = self.IntrospectGRU.introspect(conv_padded)
         for layer in range(self.RNN.num_layers):
             result['rnn{}'.format(layer)] = [ rnn[i, layer, :l[i], :] for i in range(len(rnn)) ]
-        
+
         logging.info("Computing aggregated and normalized encoding")
         x, hx = self.RNN(conv_padded)
         x, _lens = nn.utils.rnn.pad_packed_sequence(x, batch_first = True)
         x = nn.functional.normalize(self.att(x), p=2, dim=1)
         result['att'] = list(x)
         return result
-    
+
 def inout(Conv, L):
         """Mapping from size of input to the size of the output of a 1D convolutional layer.
         https://pytorch.org/docs/stable/nn.html#torch.nn.Conv1d
@@ -77,7 +77,7 @@ def inout(Conv, L):
         ksize  = Conv.kernel_size[0]
         stride = Conv.stride[0]
         return ( (L.float() + 2*pad - 1*(ksize-1) -1) / stride + 1).floor().long()
-    
+
 class Attention(nn.Module):
     def __init__(self, in_size, hidden_size):
         super(Attention, self).__init__()
@@ -93,5 +93,3 @@ class Attention(nn.Module):
         x = torch.sum(self.alpha * input, 1)
         # return the resulting embedding
         return x
-    
-    
