a59e68f52bcec09308346f029ec5bfb41a9e7c8e
Grzegorz Chrupała <g.chrupala@uvt.nl>
2019-11-07 13:34:16 +0100

    Refactor analysis/phonemes.py

--- platalea/analysis/phoneme.py
+++ platalea/analysis/phoneme.py
@@ -9,7 +9,7 @@ import sys
 import torch.nn as nn
 import platalea.basic as basic
 import platalea.encoders as encoders
-import platalea.dataset as dataset
+import platalea.attention
 import os.path
 import logging
 import json
@@ -21,20 +21,7 @@ import ursa.util as U
 import pickle
 
 
-def prepare():
-    logging.getLogger().setLevel('INFO')
-    logging.info("Loading pytorch models")
-    net_rand = basic.SpeechImage(basic.DEFAULT_CONFIG).cuda()
-    net_train = basic.SpeechImage(basic.DEFAULT_CONFIG)
-    net_train.load_state_dict(torch.load("experiments/basic-stack/net.20.pt").state_dict())
-    net_train.cuda()
-    nets = [('trained', net_train), ('random', net_rand)]
-    # Recurrent
-    for rep, net in nets:
-        with torch.no_grad():
-            net.eval()
-            save_phoneme_data([(rep, net)], batch_size=32, framewise=True) 
-            
+
 
 ## Models            
 ### Local
@@ -42,18 +29,19 @@ def prepare():
 def local_diagnostic():
     logging.getLogger().setLevel('INFO')
     output = []
+    data_mfcc = pickle.load(open('local_input.pkl', 'rb'))
     for rep in ['trained', 'random']:
-        data = pickle.load(open('phoneme_data_{}.pkl'.format(rep), 'rb'))
+        data = pickle.load(open('local_{}.pkl'.format(rep), 'rb'))
         logging.info("Fitting Logistic Regression for mfcc")
-        result  = logreg_acc_adam(data['mfcc']['features'], data['mfcc']['labels'], epochs=40, device='cuda:0')
+        result  = logreg_acc_adam(data_mfcc['features'], data_mfcc['labels'], epochs=40, device='cuda:0')
         logging.info("Result for {}, {} = {}".format(rep, 'mfcc', result['acc']))
         #np.save('logreg_w_{}_{}.npy'.format(rep, 'mfcc'), w)
         result['model'] = rep
         result['layer'] = 'mfcc'
         output.append(result)
-        for kind in data[rep]:
+        for kind in data:
             logging.info("Fitting Logistic Regression for {}, {}".format(rep, kind))
-            result = logreg_acc_adam(data[rep][kind]['features'], data[rep][kind]['labels'], epochs=40, device='cuda:0')
+            result = logreg_acc_adam(data[kind]['features'], data[kind]['labels'], epochs=40, device='cuda:0')
             logging.info("Result for {}, {} = {}".format(rep, kind, result['acc']))
             result['model'] = rep
             result['layer'] = kind
@@ -116,8 +104,8 @@ def global_rsa_plot():
     ggsave(g, "global_rsa.png")
 
 ## Tables
-def learning_sensitivity():
-    def sens(trained, random): 
+def learning_effect():
+    def le(trained, random): 
         return 1 - (random/trained) 
     ld = pd.read_json("local_diagnostic.json", orient="records");    ld['scope'] = 'local';   ld['method'] = 'diagnostic'
     lr = pd.read_json("local_rsa.json", orient="records");           lr['scope'] = 'local';   lr['method'] = 'rsa'     
@@ -128,103 +116,13 @@ def learning_sensitivity():
     data['score'] = data['rer'].fillna(0.0) + data['cor'].fillna(0.0) 
     trained = data.loc[data['model']=='trained']
     random  = data.loc[data['model']=='random']
-    trained['sensitivity'] = sens(trained['score'].values, random['score'].values)
-    data = trained[['epoch', 'layer', 'method', 'scope', 'score', 'sensitivity']].reset_index()
+    trained['learning_effect'] = le(trained['score'].values, random['score'].values)
+    data = trained[['epoch', 'layer', 'method', 'scope', 'score', 'learning_effect']].reset_index()
     order = ['mfcc', 'conv', 'rnn0', 'rnn1', 'rnn2', 'rnn3']
     data['layer_id'] = [ order.index(x) for x in data['layer'] ] 
-    json.dump(data.to_dict(orient='records'), open('learning_sensitivity.json', 'w'))
-    g = ggplot(data, aes(x='layer_id', y='sensitivity', color='method', linetype='scope')) + geom_point(size=2) + geom_line(size=1) 
-    ggsave(g, "learning_sensitivity.png")
-    
-def save_phoneme_data(nets,  batch_size, framewise=True):
-    """Generate data for training a phoneme decoding model."""
-
-    alignment_path="/roaming/gchrupal/datasets/flickr8k/dataset.val.fa.json"    
-    logging.info("Loading alignments")
-    data = {}
-    for line in open(alignment_path):
-        item = json.loads(line)
-        item['audio_id'] = os.path.basename(item['audiopath'])
-        data[item['audio_id']] = item
-    logging.info("Loading audio features")
-    val = dataset.Flickr8KData(root='/roaming/gchrupal/datasets/flickr8k/', split='val')
-    # 
-    alignments_all = [ data[sent['audio_id']] for sent in val ]
-    # Only consider cases where alignement does not fail
-    alignments = [ item for item in alignments_all if np.all([word.get('start', False) for word in item['words']]) ]
-    sentids = set(item['audio_id'] for item in alignments)
-    audio = [ sent['audio'] for sent in val if sent['audio_id'] in sentids ]
-    audio_np = [ a.numpy() for a in audio]
-    phon  = [ {'audio_id': datum['audio_id'],
-               'ipa': align2ipa(datum),
-               'transcript': datum['transcript'],
-               'audio': au } for datum, au in zip(alignments, audio_np) ]
-    logging.info("Saving IPA transcriptions")
-    np.save("transcription.val.npy", phon)
-    result = {}
-    logging.info("Computing data for MFCC")
-
-    y, X = phoneme_activations(audio_np, alignments, index=lambda ms: ms//10, framewise=framewise)
-    result['mfcc'] = check_nan(features=X, labels=y)
-    for name, net in nets:
-        result[name] = {}
-        index = lambda ms: encoders.inout(net.SpeechEncoder.Conv, torch.tensor(ms)//10).numpy()
-        try:
-            logging.info("Loading activations from activations.val.{}.npy".format(name))
-            activations = np.load("activations.val.{}.npy".format(name), allow_pickle=True).item()
-        except FileNotFoundError:    
-            logging.info("Computing data for {}".format(name))
-            activations = collect_activations(net, audio, batch_size=batch_size)
-            activations['audio_id'] = np.array([ item['audio_id'] for item in phon ])
-            logging.info("Saving activations to activations.val.{}.npy".format(name))
-            np.save("activations.val.{}.npy".format(name), activations)
-        for key in ['conv', 'rnn0', 'rnn1', 'rnn2', 'rnn3']:
-                logging.info("Computing data for {}, {}".format(name, key))
-                y, X = phoneme_activations(activations[key], alignments, index=index)
-                result[name][key] = check_nan(features=X, labels=y)
-    pickle.dump(result, open('phoneme_data_{}.pkl'.format(rep), 'wb'), protocol=4)    
-
-def align2ipa(datum):
-    """Extract IPA transcription from alignment information for a sentence."""
-    from platalea.analysis.ipa import arpa2ipa
-    result = []
-    for word in datum['words']:
-        for phoneme in word['phones']:
-            result.append(arpa2ipa(phoneme['phone'].split('_')[0], '_'))
-    return ''.join(result)
-        
-def collect_activations(net, audio, batch_size=32):
-    data = torch.utils.data.DataLoader(dataset=audio,
-                                       batch_size=batch_size,
-                                       shuffle=False,
-                                       num_workers=0,
-                                       collate_fn=dataset.batch_audio)
-    out = {}
-    for au, l in data:
-        act = net.SpeechEncoder.introspect(au.cuda(), l.cuda())
-        for k in act:
-            if k not in out:
-                out[k] = []
-            out[k]  += [ item.detach().cpu().numpy() for item in act[k] ]
-    return { k: np.array(v) for k,v in out.items() }
-    
-
-def phoneme_activations(activations, alignments, index=lambda ms: ms//10, framewise=True):
-    """Return array of phoneme labels and array of corresponding mean-pooled activation states."""
-    labels = []
-    states = []
-    for activation, alignment in zip(activations, alignments):
-        # extract phoneme labels and activations for current utterance
-        if framewise:
-            y, X = zip(*list(frames(alignment, activation, index=index)))
-        else:
-            y, X = zip(*list(slices(alignment, activation, index=index)))
-        y = np.array(y)
-        X = np.stack(X)
-        labels.append(y)
-        states.append(X)
-    return np.concatenate(labels), np.concatenate(states)
-
+    json.dump(data.to_dict(orient='records'), open('learning_effect.json', 'w'))
+    g = ggplot(data, aes(x='layer_id', y='learning_effect', color='method', linetype='scope')) + geom_point(size=2) + geom_line(size=1) 
+    ggsave(g, "learning_effect.png")
 
 def majority_binary(y):
     return (y.mean(dim=0) >= 0.5).float()
@@ -252,50 +150,6 @@ def logreg_acc_adam(features, labels, test_size=1/2, epochs=1, device='cpu'):
     result = train_classifier(model, X, y, X_val, y_val, epochs=epochs, majority=majority_multiclass)
     return result
 
-def slices(utt, rep, index, aggregate=lambda x: x.mean(axis=0)):
-    """Return sequence of slices associated with phoneme labels, given an
-       alignment object `utt`, a representation array `rep`, and
-       indexing function `index`, and an aggregating function\
-       `aggregate`.
-    """
-    for phoneme in phones(utt):
-        phone, start, end = phoneme
-        assert index(start)<index(end)+1, "Something funny: {} {} {} {}".format(start, end, index(start), index(end))
-        yield (phone, aggregate(rep[index(start):index(end)+1]))
-
-def frames(utt, rep, index): 
-     """Return pair sequence of (phoneme label, frame), given an 
-        alignment object `utt`, a representation array `rep`, and 
-       indexing function `index`. 
-     """ 
-     for phoneme in phones(utt): 
-         phone, start, end = phoneme 
-         assert index(start)<index(end)+1, "Something funny: {} {} {} {}".format(start, end, index(start), index(end)) 
-         for j in range(index(start), index(end)+1): 
-             yield (phone, rep[j])
-             
-def phones(utt):
-    """Return sequence of phoneme labels associated with start and end
-     time corresponding to the alignment JSON object `utt`.
-    
-    """
-    for word in utt['words']:
-        pos = word['start']
-        for phone in word['phones']:
-            start = pos
-            end = pos + phone['duration']
-            pos = end
-            label = phone['phone'].split('_')[0]
-            if label != 'oov':
-                yield (label, int(start*1000), int(end*1000))
-                
-def check_nan(labels, features):
-    # Get rid of NaNs
-    ix = np.isnan(features.sum(axis=1))
-    logging.info("Found {} NaNs".format(sum(ix)))
-    X = features[~ix]
-    y = labels[~ix]
-    return dict(features=X, labels=y)
 
 def weight_variance():
     kinds = ['rnn0', 'rnn1', 'rnn2', 'rnn3']
@@ -328,15 +182,14 @@ def framewise_RSA(test_size=1/2):
     splitseed = 123
     result = []
     mfcc_done = False
+    data = pickle.load(open("local_input.pkl", "rb"))
     for mode in ["trained", "random"]:
-        logging.info("Loading phoneme data for {}".format(mode))
-        data = pickle.load(open("phoneme_data_{}.pkl".format(mode), "rb"))
         mfcc_cor = [ item['cor']  for item in result if item['layer'] == 'mfcc']
         if len(mfcc_cor) > 0:
             logging.info("Result for MFCC computed previously")
             result.append(dict(model=mode, layer='mfcc', cor=mfcc_cor[0]))
         else:
-            X, X_val, y, y_val = train_test_split(data['mfcc']['features'], data['mfcc']['labels'], test_size=test_size, random_state=splitseed)
+            X, X_val, y, y_val = train_test_split(data['features'], data['labels'], test_size=test_size, random_state=splitseed)
             logging.info("Computing label identity matrix for {} datapoints".format(len(y_val)))
             # y_sim = y.reshape((-1, 1)) == y
             y_val_sim = torch.tensor(y_val.reshape((-1, 1)) == y_val).float()
@@ -347,8 +200,10 @@ def framewise_RSA(test_size=1/2):
             cor = S.pearson_r(S.triu(y_val_sim), S.triu(X_val_sim)).item()
             logging.info("Point biserial correlation for {}, mfcc: {}".format(mode, cor))
             result.append(dict(model=mode, layer='mfcc', cor=cor))
+        logging.info("Loading phoneme data for {}".format(mode))
+        data = pickle.load(open("local_{}.pkl".format(mode), "rb"))
         for layer in ['conv', 'rnn0', 'rnn1', 'rnn2', 'rnn3']:
-                X, X_val, y, y_val = train_test_split(data[mode][layer]['features'], data[mode][layer]['labels'], test_size=test_size, random_state=splitseed)
+                X, X_val, y, y_val = train_test_split(data[layer]['features'], data[layer]['labels'], test_size=test_size, random_state=splitseed)
                 logging.info("Computing label identity matrix for {} datapoints".format(len(y_val)))
                 # y_sim = y.reshape((-1, 1)) == y
                 y_val_sim = torch.tensor(y_val.reshape((-1, 1)) == y_val).float()
@@ -400,9 +255,9 @@ def weighted_average_RSA(scalar=True, test_size=1/2, hidden_size=1024, epochs=1,
     splitseed = 123
     result = []
     logging.info("Loading transcription data")
-    data = np.load("transcription.val.npy", allow_pickle=True)
-    trans = [ datum['ipa'] for datum in data ]
-    act = [ torch.tensor([item['audio'][:, :]]).float().to(device) for item in data ]
+    data = pickle.load(open("global_input.pkl", "rb"))
+    trans = data['ipa']
+    act = [ torch.tensor([item[:, :]]).float().to(device) for item in data['audio'] ]
 
     trans, trans_val, act, act_val = train_test_split(trans, act, test_size=test_size, random_state=splitseed) 
 
@@ -417,7 +272,7 @@ def weighted_average_RSA(scalar=True, test_size=1/2, hidden_size=1024, epochs=1,
     logging.info("Maximum correlation on val: {} at epoch {}".format(result[-1]['cor'], result[-1]['epoch']))
     for mode in ["random", "trained"]:
         logging.info("Loading activations for {} data".format(mode))
-        data = np.load("activations.val.{}.npy".format(mode), allow_pickle=True).item()
+        data = pickle.load(open("global_{}.pkl".format(mode), "rb"))
         for layer in ['conv', 'rnn0', 'rnn1', 'rnn2', 'rnn3']:
             logging.info("Training for {} {}".format(mode, layer))
             act = [ torch.tensor([item[:, :]]).float().to(device) for item in data[layer] ]
@@ -430,12 +285,11 @@ def weighted_average_RSA(scalar=True, test_size=1/2, hidden_size=1024, epochs=1,
 
     
 def train_wa(edit_sim, edit_sim_val, stack, stack_val, scalar=True, hidden_size=1024, epochs=1, device='cpu'):
-    import platalea.encoders
     if scalar:
-        wa = platalea.encoders.ScalarAttention(stack[0].size(2), hidden_size).to(device)
+        wa = platalea.attention.ScalarAttention(stack[0].size(2), hidden_size).to(device)
     else:
         # This crashes CUDA memory
-        wa = platalea.encoders.Attention(stack[0].size(2), hidden_size).to(device)
+        wa = platalea.attention.Attention(stack[0].size(2), hidden_size).to(device)
     optim = torch.optim.Adam(wa.parameters())
     minloss = 0; minepoch = None
     logging.info("Optimizing for {} epochs".format(epochs))
@@ -467,10 +321,10 @@ def weighted_average_diagnostic(attention='scalar', test_size=1/2, hidden_size=1
     splitseed = 123
     result = []
     logging.info("Loading transcription data")
-    data = np.load("transcription.val.npy", allow_pickle=True)
-    trans = [ datum['ipa'] for datum in data ]
-    act = [ torch.tensor(item['audio'][:, :]).float() for item in data ]
-
+    data = pickle.load(open("global_input.pkl", "rb"))
+    trans = data['ipa']
+    act = [ torch.tensor(item[:, :]).float().to(device) for item in data['audio'] ]
+    
     trans, trans_val, X, X_val = train_test_split(trans, act, test_size=test_size, random_state=splitseed) 
 
     logging.info("Computing targets")
@@ -488,7 +342,7 @@ def weighted_average_diagnostic(attention='scalar', test_size=1/2, hidden_size=1
     logging.info("Maximum accuracy on val: {} at epoch {}".format(result[-1]['acc'], result[-1]['epoch']))
     for mode in ["random", "trained"]:
         logging.info("Loading activations for {} data".format(mode))
-        data = np.load("activations.val.{}.npy".format(mode), allow_pickle=True).item()
+        data = pickle.load(open("global_{}.pkl".format(mode), "rb"))
         for layer in ['conv', 'rnn0', 'rnn1', 'rnn2', 'rnn3']:
             logging.info("Training for {} {}".format(mode, layer))
             act = [ torch.tensor(item[:, :]).float() for item in data[layer] ]
@@ -586,11 +440,11 @@ class PooledClassifier(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, attention='scalar'):
         super(PooledClassifier, self).__init__()
         if attention == 'scalar':
-            self.wa = encoders.ScalarAttention(input_size, hidden_size)
+            self.wa = platalea.attention.ScalarAttention(input_size, hidden_size)
         elif attention == 'linear':
-            self.wa = encoders.LinearAttention(input_size)
+            self.wa = platalea.attention.LinearAttention(input_size)
         else:
-            self.wa = encoders.Attention(input_size, hidden_size)
+            self.wa = platalea.attention.Attention(input_size, hidden_size)
         self.project = nn.Linear(in_features=input_size, out_features=output_size)
         self.loss = nn.functional.binary_cross_entropy_with_logits
         
