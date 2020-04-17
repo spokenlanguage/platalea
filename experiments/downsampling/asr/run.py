5faa8360784b30e92f8799e6f953da705438854e
Bertrand Higy <bertrand.higy@gmail.com>
2020-04-17 17:32:25 +0200

    using get_default_config() for ASR model for consistent config

--- experiments/downsampling/asr/run.py
+++ experiments/downsampling/asr/run.py
@@ -2,7 +2,6 @@ import logging
 import pickle
 from shutil import copyfile
 import torch
-import torch.nn as nn
 
 import platalea.asr as M
 import platalea.dataset as D
@@ -10,10 +9,7 @@ from utils.copy_best import copy_best
 
 torch.manual_seed(123)
 
-
 batch_size = 8
-hidden_size = 1024
-dropout = 0.0
 feature_fname = 'mfcc_delta_features.pt'
 
 logging.basicConfig(level=logging.INFO)
@@ -37,32 +33,8 @@ for ds_factor in factors:
                      language='en'),
                 open('config.pkl', 'wb'))
 
-    config = dict(
-        SpeechEncoder=dict(
-            conv=dict(in_channels=39, out_channels=64, kernel_size=6, stride=2,
-                      padding=0, bias=False),
-            rnn=dict(input_size=64, hidden_size=hidden_size, num_layers=6,
-                     bidirectional=True, dropout=dropout),
-            rnn_layer_type=nn.GRU),
-        TextDecoder=dict(
-            emb=dict(num_embeddings=fd.vocabulary_size(),
-                     embedding_dim=hidden_size),
-            drop=dict(p=dropout),
-            att=dict(in_size_enc=hidden_size * 2, in_size_state=hidden_size,
-                     hidden_size=hidden_size),
-            rnn=dict(input_size=hidden_size * 3, hidden_size=hidden_size,
-                     num_layers=1, dropout=dropout),
-            out=dict(in_features=hidden_size * 3,
-                     out_features=fd.vocabulary_size()),
-            rnn_layer_type=nn.GRU,
-            max_output_length=400,  # max length for flickr annotations is 199
-            sos_id=fd.get_token_id(fd.sos),
-            eos_id=fd.get_token_id(fd.eos),
-            pad_id=fd.get_token_id(fd.pad)),
-        inverse_transform_fn=fd.get_label_encoder().inverse_transform)
-
     logging.info('Building model')
-    net = M.SpeechTranscriber(config)
+    net = M.SpeechTranscriber(M.get_default_config())
     run_config = dict(max_norm=2.0, max_lr=2 * 1e-4, epochs=32, opt='adam')
 
     logging.info('Training')
