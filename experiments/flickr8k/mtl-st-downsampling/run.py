1f4c9a0bb7a5c92de25c029c4b1be9e36673a70f
Bertrand Higy <bertrand.higy@gmail.com>
2020-05-06 16:11:00 +0200

    fixed merging issues

--- experiments/flickr8k/mtl-st-downsampling/run.py
+++ experiments/flickr8k/mtl-st-downsampling/run.py
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import configargparse
 import logging
-import pickle
 import random
 from shutil import copyfile
 import torch
-=======
-=======
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
-import logging
-import pickle
-from shutil import copyfile
-import torch
 import torch.nn as nn
-<<<<<<< HEAD
->>>>>>> Using np.memmap files for librispeech instead of pytorch
-=======
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
 
 import platalea.dataset as D
 import platalea.mtl as M
 from platalea.score import score, score_speech_text
 from utils.copy_best import copy_best
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 # Parsing arguments
 parser = configargparse.get_argument_parser('platalea')
 parser.add_argument(
@@ -38,28 +22,10 @@ torch.manual_seed(config_args.seed)
 random.seed(config_args.seed)
 logging.basicConfig(level=logging.INFO)
 
-=======
-torch.manual_seed(123)
->>>>>>> Using np.memmap files for librispeech instead of pytorch
-=======
-torch.manual_seed(123)
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
 
 batch_size = 8
 hidden_size = 1024
 dropout = 0.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-feature_fname = 'mfcc_delta_features.pt'
-
-logging.basicConfig(level=logging.INFO)
->>>>>>> Using np.memmap files for librispeech instead of pytorch
-=======
-feature_fname = 'mfcc_delta_features.pt'
-
-logging.basicConfig(level=logging.INFO)
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
 
 factors = [3, 9, 27, 81, 243]
 lz = len(str(abs(factors[-1])))
@@ -67,31 +33,10 @@ for ds_factor in factors:
     logging.info('Loading data')
     data = dict(
         train=D.flickr8k_loader(split='train', batch_size=batch_size,
-<<<<<<< HEAD
-<<<<<<< HEAD
                                 shuffle=True, downsampling_factor=ds_factor),
         val=D.flickr8k_loader(split='val', batch_size=batch_size,
                               shuffle=False))
     fd = D.Flickr8KData
-=======
-=======
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
-                                shuffle=True, feature_fname=feature_fname,
-                                downsampling_factor=ds_factor),
-        val=D.flickr8k_loader(split='val', batch_size=batch_size,
-                              shuffle=False, feature_fname=feature_fname))
-    fd = D.Flickr8KData
-    fd.init_vocabulary(data['train'].dataset)
-
-    # Saving config
-    pickle.dump(dict(feature_fname=feature_fname,
-                     label_encoder=fd.get_label_encoder(),
-                     language='en'),
-                open('config.pkl', 'wb'))
-<<<<<<< HEAD
->>>>>>> Using np.memmap files for librispeech instead of pytorch
-=======
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
 
     config = dict(
         SharedEncoder=dict(
@@ -121,15 +66,7 @@ for ds_factor in factors:
 
     logging.info('Building model')
     net = M.MTLNetSpeechText(config)
-<<<<<<< HEAD
-<<<<<<< HEAD
     run_config = dict(max_norm=2.0, max_lr=2 * 1e-4, epochs=32)
-=======
-    run_config = dict(max_norm=2.0, max_lr=2 * 1e-4, epochs=32, opt='adam')
->>>>>>> Using np.memmap files for librispeech instead of pytorch
-=======
-    run_config = dict(max_norm=2.0, max_lr=2 * 1e-4, epochs=32, opt='adam')
->>>>>>> 2b7826d4f0d14d7d1bf6fd91894066a762034a29
 
     tasks = [
         dict(name='SI', net=net.SpeechImage, data=data, eval=score),
