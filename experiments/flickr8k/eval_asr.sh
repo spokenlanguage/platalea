99dcebf1d4deac7c7633b6ca157610a21f474155
Bertrand Higy <bertrand.higy@gmail.com>
2020-06-19 17:01:46 +0200

    moved remaining elements to new exp folder or textual-supervision repo

--- experiments/flickr8k/eval_asr.sh
+++
@@ -1,8 +0,0 @@
-SCRIPTPATH=$(readlink -f "$0")
-SCRIPTDIR=$(dirname "$SCRIPTPATH")
-
-for exp in runs/asr*-ds*; do
-    cd $exp
-    python $SCRIPTDIR/../../utils/evaluate_net.py -b net.best.pt >result_beam.json
-    cd - > /dev/null
-done
