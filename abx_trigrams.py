6efad1eb09c062e26f6b624920ce5687a3696172
Bertrand Higy <bertrand.higy@gmail.com>
2020-07-13 16:51:25 +0200

    refactored to faciliate evaluation of Niekerk's model

--- abx_trigrams.py
+++ abx_trigrams.py
@@ -4,21 +4,17 @@ import os.path
 from pathlib import Path
 import json
 import platalea.ipa as ipa
-import h5py
 import logging
-import zerospeech2020.evaluation.abx as abx
 from ABXpy.misc.any2h5features import convert
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import shutil
-import tempfile
 import ABXpy
 import ABXpy.task
 from zerospeech2020.evaluation.abx import _load_features_2019, _average
 import ABXpy.distances.distances
 from ABXpy.distance import edit_distance
 import ABXpy.score as score
-import ABXpy.misc.items as items
 import ABXpy.analyze as analyze
 import pandas as pd
 import torch
@@ -37,7 +33,8 @@ def phonemes(u):
                 current = phone['end']
                 result.append(phone)
     return result
-    
+
+
 def trigrams(xs):
     if len(xs) < 3:
         return []
@@ -46,7 +43,7 @@ def trigrams(xs):
 
 
 def deoov(xs):
-    return [ x for x in xs if not any(xi['phone'].startswith('oov') or xi['phone'].startswith('sil') for xi in x) ]
+    return [x for x in xs if not any(xi['phone'].startswith('oov') or xi['phone'].startswith('sil') for xi in x)]
 
 
 class Topline:
@@ -58,20 +55,21 @@ class Topline:
         self.oh.fit(phonemes)
 
     def transform(self, xs):
-        features = np.array(xs).reshape(-1,1)
+        features = np.array(xs).reshape(-1, 1)
         return self.oh.transform(features)
 
     def dump(self, xs, path):
         np.savetxt(path, self.transform(xs))
-            
+
+
 def prepare_abx(k=1000):
     import csv
     align = {}
     for line in open("data/datasets/flickr8k/fa.json"):
         u = json.loads(line)
-        u['audiopath'] =  os.path.split(u['audiopath'])[-1]
+        u['audiopath'] = os.path.split(u['audiopath'])[-1]
         align[u['audiopath']] = u
-    us = random.sample(list(align.values()),k)
+    us = random.sample(list(align.values()), k)
     wav = Path("data/datasets/flickr8k/flickr_audio/wavs")
     out = Path("data/flickr8k_abx_wav")
     speakers = dict(line.split() for line in open("data/datasets/flickr8k/wav2spk.txt"))
@@ -79,7 +77,7 @@ def prepare_abx(k=1000):
     with open("data/flickr8k_abx.item", "w") as itemout:
         items = csv.writer(itemout, delimiter=' ', lineterminator='\n')
         items.writerow(["#file", "onset", "offset", "#phone", "speaker", "context"])
-        for u in us:        
+        for u in us:
             filename = os.path.split(u['audiopath'])[-1]
             speaker = speakers[filename]
             bare, _ = os.path.splitext(filename)
@@ -90,35 +88,36 @@ def prepare_abx(k=1000):
             for i, gram in enumerate(grams):
                 start = int(gram[0]['start']*1000)
                 end = int(gram[-1]['end']*1000)
-                triple = [ phone['phone'].split('_')[0] for phone in gram ]
-                fragment = sound[start : end]
+                triple = [phone['phone'].split('_')[0] for phone in gram]
+                fragment = sound[start: end]
                 target = out / "{}_{}.wav".format(bare, i)
                 if end - start < 100:
                     logging.info("SKIPPING short audio {}".format(target))
                 else:
-                    items.writerow([ "{}_{}".format(bare,i), 0, end-start, triple[1], speaker, '_'.join([triple[0], triple[-1]])])
+                    items.writerow(["{}_{}".format(bare, i), 0, end-start, triple[1], speaker, '_'.join([triple[0], triple[-1]])])
                     fragment.export(format='wav', out_f=target)
                     logging.info("Saved {}th trigram in {}".format(i, target))
-    
+
     task = ABXpy.task.Task("data/flickr8k_abx.item", "phone", by="context", across="speaker")
     logging.info("Task statistics: {}".format(task.stats))
     logging.info("Generating triplets")
     if os.path.isfile("data/flickr8k_abx.triplets"):
         os.remove("data/flickr8k_abx.triplets")
-    task.generate_triplets(output= "data/flickr8k_abx.triplets")
+    task.generate_triplets(output="data/flickr8k_abx.triplets")
 
 
-def ed(x, y, normalized=None): 
-    return edit_distance(x, y) 
+def ed(x, y, normalized=None):
+    return edit_distance(x, y)
+
 
 def run_abx(feature_dir, triplet_file):
     root, _ = os.path.split(feature_dir)
     logging.info("Converting features {}".format(feature_dir))
-    convert(feature_dir, root +  "/features", load=_load_features_2019)
+    convert(feature_dir, root + "/features", load=_load_features_2019)
     logging.info("Computing distances")
-    ABXpy.distances.distances.compute_distances( 
-            root +  "/features",
-            'features', 
+    ABXpy.distances.distances.compute_distances(
+            root + "/features",
+            'features',
             triplet_file,
             root + "/distance",
             ed,
@@ -126,11 +125,21 @@ def run_abx(feature_dir, triplet_file):
             n_cpu=16)
     logging.info("Computing scores")
     score.score(triplet_file, root + "/distance", root + "/score")
-    analyze.analyze(triplet_file, root + "/score", root +  "/analyze")
+    analyze.analyze(triplet_file, root + "/score", root + "/analyze")
     data = pd.read_csv(root + "/analyze", delimiter='\t')
     return data
-    
-        
+
+
+def compute_result(encoded_dir, triplets_fpath, output_dir):
+    result = run_abx(encoded_dir, triplets_fpath)
+    result.to_csv("{}/abx_flickr8k_analyze.csv".format(output_dir),
+                  sep='\t', header=True, index=False)
+    avg_error = _average("{}/abx_flickr8k_analyze.csv".format(output_dir),
+                         "across")
+    json.dump(dict(avg_abx_error=avg_error),
+              open("{}/abx_flickr8k_result.json".format(output_dir), "w"))
+    return avg_error
+
 
 def abx(k=1000):
     from platalea.vq_encode import encode
@@ -140,7 +149,7 @@ def abx(k=1000):
     for modeldir in experiments("abx_flickr8k_result.json"):
     # for modeldir in ["experiments/vq-32-q1/"]:
     #for modeldir in ["experiments/vq-512-q1/"]:
-        result = [ json.loads(line) for line in open(modeldir + "result.json") ]
+        result = [json.loads(line) for line in open(modeldir + "result.json")]
         best = sorted(result, key=lambda x: x['recall']['10'], reverse=True)[0]['epoch']
         oldnet = torch.load("{}/net.{}.pt".format(modeldir, best))
         logging.info("Loading model from {} at epoch {}".format(modeldir, best))
@@ -153,18 +162,16 @@ def abx(k=1000):
         logging.info("Encoding data")
         encode(net, "data/flickr8k_abx_wav/", encoded_dir)
         logging.info("Computing ABX")
-        result = run_abx(encoded_dir, "data/flickr8k_abx.triplets")
-        result.to_csv("{}/abx_flickr8k_analyze.csv".format(modeldir), sep='\t', header=True, index=False)
-        avg_error = _average("{}/abx_flickr8k_analyze.csv".format(modeldir), "across")
-        json.dump(dict(avg_abx_error=avg_error), open("{}/abx_flickr8k_result.json".format(modeldir), "w"))
+        avg_error = compute_result(encoded_dir, "data/flickr8k_abx.triplets",
+                                   modeldir)
         logging.info("Score: {}".format(avg_error))
-        
+
+
 def main():
     random.seed(123)
     logging.basicConfig(level=logging.INFO)
     abx(k=1000)
-    
+
+
 if __name__ == '__main__':
     main()
-    
-
