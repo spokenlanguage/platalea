570fdedb89ba0c869233149faf6171f24e5b556b
Bertrand Higy <bertrand.higy@gmail.com>
2020-09-09 18:15:06 +0200

    refactored plotting

--- plot_niekerk.py
+++ plot_niekerk.py
@@ -1,95 +1,149 @@
-from extract_results import extract_abx, extract_rsa, extract_dc, range_size
-import matplotlib.pyplot as plt
-import numpy as np
+import glob
+import json
+from pathlib import Path
+from plot import from_records, select
+import plotnine as pn
 
 
-abx = extract_abx()
-rsa = extract_rsa()
-dc = extract_dc()
+def extract_abx(fpath):
+    if fpath.exists():
+        return 100 - json.load(open(fpath))['avg_abx_error']
+    else:
+        return None
 
-# ABX
-xrange = range_size
-fig, ax = plt.subplots()
-legend = []
-for type in ['z', 'indices']:
+
+def extract_dc(fpath, mode):
+    if fpath.exists():
+        return select(fpath, dict(model=mode))['acc']
+    else:
+        return None
+
+
+def extract_rsa(fpath, mode, ref):
+    if fpath.exists():
+        return select(fpath, dict(model=mode, reference=ref))['cor']
+    else:
+        return None
+
+
+def extract_results_niekerk():
+    results = []
+    for path in glob.glob('experiments/niekerk/*/val/*/*'):
+        d = Path(path)
+        s = path.split('/')
+        results.append(dict(
+            size=s[4],
+            input_type=('sentences' if s[2] == 'english' else 'trigrams'),
+            output_type=('embeddings' if s[5] == 'z' else 'indices'),
+            abx_base=extract_abx(d / 'random/flickr8k_abx_within_result.json'),
+            abx=extract_abx(d / 'trained/flickr8k_abx_within_result.json'),
+            dc_base=extract_dc(d / 'local/local_diagnostic.json', 'random'),
+            dc=extract_dc(d / 'local/local_diagnostic.json', 'trained'),
+            rsa_base=extract_rsa(d / 'ed_rsa.json', 'random', 'phoneme'),
+            rsa_base_w=extract_rsa(d / 'ed_rsa.json', 'random', 'word'),
+            rsa=extract_rsa(d / 'ed_rsa.json', 'trained', 'phoneme'),
+            rsa_w=extract_rsa(d / 'ed_rsa.json', 'trained', 'word'),
+            rsa3_base=extract_rsa(d / 'ed_rsa.json', 'random', 'phoneme'),
+            rsa3_base_w=extract_rsa(d / 'ed_rsa_trigrams.json', 'random', 'word'),
+            rsa3=extract_rsa(d / 'ed_rsa_trigrams.json', 'trained', 'phoneme'),
+            rsa3_w=extract_rsa(d / 'ed_rsa_trigrams.json', 'trained', 'word')))
+    return results
+
+
+def main():
+    res = extract_results_niekerk()
+    res = [r for r in res if r['abx'] is not None and r['rsa3'] is not None]
+    res = from_records(res)
+    p = pn.ggplot(res, pn.aes(x='abx', y='rsa3')) + \
+            pn.geom_point(pn.aes(color='factor(size)'))
+    pn.ggsave(p, 'vn_abx_rsa3.pdf')
+    return
+    # ABX
+    xrange = range_size
+    fig, ax = plt.subplots()
+    legend = []
+    for type in ['z', 'indices']:
+        for mode in ['random', 'trained']:
+            data = [100 - abx['english_triplets'][x][type][mode] for x in xrange]
+            ax.plot(range(0, len(xrange)), data)
+            legend.append('{} - {}'.format(type, mode))
+            ax.set(xlabel='Size of the codebook', ylabel='ABX accuracy')
+    ax.set_xticks(np.arange(len(xrange)))
+    ax.set_xticklabels(xrange)
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_abx_triplets.pdf")
+    fig, ax = plt.subplots()
+    legend = []
+    for type in ['z', 'indices']:
+        for mode in ['random', 'trained']:
+            data = [100 - abx_within['english_triplets'][x][type][mode] for x in xrange]
+            ax.plot(range(0, len(xrange)), data)
+            legend.append('{} - {}'.format(type, mode))
+            ax.set(xlabel='Size of the codebook', ylabel='ABX accuracy')
+    ax.set_xticks(np.arange(len(xrange)))
+    ax.set_xticklabels(xrange)
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_abx_within_triplets.pdf")
+
+    # RSA
+    fig, ax = plt.subplots()
+    legend = []
     for mode in ['random', 'trained']:
-        data = [100 - abx['english_triplets'][x][type][mode] for x in xrange]
-        ax.plot(range(0, len(xrange)), data)
-        legend.append('{} - {}'.format(type, mode))
-        ax.set(xlabel='Size of the codebook', ylabel='ABX accuracy')
-ax.set_xticks(np.arange(len(xrange)))
-ax.set_xticklabels(xrange)
-ax.legend(legend)
-ax.grid()
-fig.savefig("abx.pdf")
+        for level in ['phoneme', 'word']:
+            data = [rsa['english_triplets'][x]['indices'][mode][level] for x in xrange]
+            ax.plot(range(0, len(xrange)), data)
+            legend.append('{} - {} - {}'.format(type, mode, level))
+            ax.set(xlabel='Size of the codebook', ylabel='RSA score')
+    ax.set_xticks(np.arange(len(xrange)))
+    ax.set_xticklabels(xrange)
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_rsa_triplets.pdf")
+    fig, ax = plt.subplots()
+    legend = []
+    for mode in ['random', 'trained']:
+        for level in ['phoneme', 'word']:
+            data = [rsa['english'][x]['indices'][mode][level] for x in xrange]
+            ax.plot(range(0, len(xrange)), data)
+            legend.append('{} - {} - {}'.format(type, mode, level))
+            ax.set(xlabel='Size of the codebook', ylabel='RSA score')
+    ax.set_xticks(np.arange(len(xrange)))
+    ax.set_xticklabels(xrange)
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_rsa.pdf")
 
-# RSA
-fig, ax = plt.subplots()
-legend = []
-for mode in ['random', 'trained']:
-    for level in ['phoneme', 'word']:
-        data = [rsa['english_triplets'][x]['indices'][mode][level] for x in xrange]
-        ax.plot(range(0, len(xrange)), data)
-        legend.append('{} - {} - {}'.format(type, mode, level))
-        ax.set(xlabel='Size of the codebook', ylabel='RSA score')
-ax.set_xticks(np.arange(len(xrange)))
-ax.set_xticklabels(xrange)
-ax.legend(legend)
-ax.grid()
-fig.savefig("rsa_triplets.pdf")
-fig, ax = plt.subplots()
-legend = []
-for mode in ['random', 'trained']:
-    for level in ['phoneme', 'word']:
-        data = [rsa['english'][x]['indices'][mode][level] for x in xrange]
-        ax.plot(range(0, len(xrange)), data)
-        legend.append('{} - {} - {}'.format(type, mode, level))
-        ax.set(xlabel='Size of the codebook', ylabel='RSA score')
-ax.set_xticks(np.arange(len(xrange)))
-ax.set_xticklabels(xrange)
-ax.legend(legend)
-ax.grid()
-fig.savefig("rsa.pdf")
+    # DC
+    fig, ax = plt.subplots()
+    legend = []
+    for mode in ['random', 'trained']:
+        for level in ['acc', 'baseline']:
+            data = [dc['english'][x]['indices'][mode][level] for x in xrange]
+            ax.plot(range(0, len(xrange)), data)
+            legend.append('{} - {} - {}'.format(type, mode, level))
+            ax.set(xlabel='Size of the codebook', ylabel='DC score')
+    ax.set_xticks(np.arange(len(xrange)))
+    ax.set_xticklabels(xrange)
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_dc.pdf")
 
-# DC
-fig, ax = plt.subplots()
-legend = []
-for mode in ['random', 'trained']:
-    for level in ['acc', 'baseline']:
-        data = [dc['english'][x]['indices'][mode][level] for x in xrange]
-        ax.plot(range(0, len(xrange)), data)
-        legend.append('{} - {} - {}'.format(type, mode, level))
-        ax.set(xlabel='Size of the codebook', ylabel='DC score')
-ax.set_xticks(np.arange(len(xrange)))
-ax.set_xticklabels(xrange)
-ax.legend(legend)
-ax.grid()
-fig.savefig("dc.pdf")
+    # DC vs. RSA
+    fig, ax = plt.subplots()
+    legend = []
+    for mode in ['trained']:
+        xdata = [dc['english'][x]['indices'][mode]['acc'] for x in xrange]
+        ydata = [rsa['english_triplets'][x]['indices'][mode]['phoneme'] for x in xrange]
+        for i in range(len(xdata)):
+            ax.plot(xdata[i], ydata[i], 'o')
+            legend.append('{}'.format(range_size[i]))
+        ax.set(xlabel='DC score', ylabel='RSA score')
+    ax.legend(legend)
+    ax.grid()
+    fig.savefig("vn_dc_rsa.pdf")
 
-# ABX vs. RSA
-fig, ax = plt.subplots()
-legend = []
-for mode in ['trained']:
-    xdata = [100 - abx['english_triplets'][x]['indices'][mode] for x in xrange]
-    ydata = [rsa['english_triplets'][x]['indices'][mode]['phoneme'] for x in xrange]
-    for i in range(len(xdata)):
-        ax.plot(xdata[i], ydata[i], 'o')
-        legend.append('{}'.format(range_size[i]))
-    ax.set(xlabel='ABX accuracy', ylabel='RSA score')
-ax.legend(legend)
-ax.grid()
-fig.savefig("abx_rsa.pdf")
 
-# DC vs. RSA
-fig, ax = plt.subplots()
-legend = []
-for mode in ['trained']:
-    xdata = [dc['english'][x]['indices'][mode]['acc'] for x in xrange]
-    ydata = [rsa['english_triplets'][x]['indices'][mode]['phoneme'] for x in xrange]
-    for i in range(len(xdata)):
-        ax.plot(xdata[i], ydata[i], 'o')
-        legend.append('{}'.format(range_size[i]))
-    ax.set(xlabel='DC score', ylabel='RSA score')
-ax.legend(legend)
-ax.grid()
-fig.savefig("dc_rsa.pdf")
+if __name__ == '__main__':
+    plot_all()
