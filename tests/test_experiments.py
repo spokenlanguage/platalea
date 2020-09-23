151af024a9c11d601d7b0171be1814368708b33d
E. G. Patrick Bos <egpbos@gmail.com>
2020-09-23 11:52:12 +0200

    add experiment.config test + fix transformer test

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,8 +1,23 @@
 import unittest.mock
 
 
+def test_config():
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]', '--epochs=2', '--flickr8k_meta=thisandthat.json',
+                                        '--audio_features_fn=mfcc_features.pt',
+                                        '--flickr8k_root=/Users/pbos/projects/spokenLanguage/flickr1d_git/flickr1d',
+                                        '--lr_scheduler=noam', '-v']):
+        from platalea.experiments.config import args
+
+        args.parse()
+
+        assert args.epochs == 2
+        assert args.flickr8k_meta == 'thisandthat.json'
+        assert args.verbose == True
+        assert args.lr_scheduler == 'noam'
+
+
 def test_transformer_experiment():
-    with unittest.mock.patch('sys.argv', ['--epochs=2', '--flickr8k_meta=dataset.json',
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]', '--epochs=2', '--flickr8k_meta=dataset.json',
                                           '--audio_features_fn=mfcc_features.pt',
                                           '--flickr8k_root=/Users/pbos/projects/spokenLanguage/flickr1d_git/flickr1d',
                                           '--lr_scheduler=noam', '-v']):
