070a77be47df1742d415faa9bc7599701cd5fe03
E. G. Patrick Bos <egpbos@gmail.com>
2021-01-26 16:05:41 +0100

    add all experiments; pip_seq fails
    This is because of the way experiments.config works in combination with how the test was written. The config module assumes it is loaded only once per run. However, because we load all experiments from a single test_experiments.py file, the config module is loaded for each test. Since pip_ind and pip_seq add the same config argument to the parser, the second one to do so causes a conflict.

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -55,3 +55,47 @@ def test_mtl_st_experiment():
                                           f'--flickr8k_root={flickr1d_path}',
                                           '--hidden_size_factor=4']):
         import platalea.experiments.flickr8k.mtl_st
+
+
+def test_asr_experiment():
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]',
+                                          '--epochs=1',
+                                          '-c', f'{flickr1d_path}/config.yml',
+                                          f'--flickr8k_root={flickr1d_path}',
+                                          '--hidden_size_factor=4']):
+        import platalea.experiments.flickr8k.asr
+    # save output of this experiment to serve as input for pip_ind and pip_seq
+
+
+def test_text_image_experiment():
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]',
+                                          '--epochs=1',
+                                          '-c', f'{flickr1d_path}/config.yml',
+                                          f'--flickr8k_root={flickr1d_path}',
+                                          '--hidden_size_factor=4']):
+        import platalea.experiments.flickr8k.text_image
+    # save output of this experiment to serve as input for pip_ind
+
+
+def test_pip_ind_experiment():
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]',
+                                          '--epochs=1',
+                                          '-c', f'{flickr1d_path}/config.yml',
+                                          f'--flickr8k_root={flickr1d_path}',
+                                          '--hidden_size_factor=4',
+                                        #   '--asr_model_dir={asr_out_path}'
+                                        #   '--text_image_model_dir={text_image_out_path}'
+                                          ]):
+        import platalea.experiments.flickr8k.pip_ind
+
+
+def test_pip_seq_experiment():
+    with unittest.mock.patch('sys.argv', ['[this gets ignored]',
+                                          '--epochs=1',
+                                          '-c', f'{flickr1d_path}/config.yml',
+                                          f'--flickr8k_root={flickr1d_path}',
+                                          '--hidden_size_factor=4',
+                                          '--pip_seq_no_beam_decoding',
+                                        #   '--asr_model_dir={asr_out_path}'
+                                          ]):
+        import platalea.experiments.flickr8k.pip_seq
