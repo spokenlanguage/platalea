d26322973ee3ee0ebfd95277f5236e4c06c70629
E. G. Patrick Bos <egpbos@gmail.com>
2021-05-26 16:55:36 +0200

    fix test expected value

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -199,7 +199,7 @@ def test_pip_ind_experiment():
 
 def test_pip_seq_experiment():
     expected = [{'medr': 1.5, 'recall': {1: 0.5, 5: 1.0, 10: 1.0},
-                 'average_loss': 0.3790612369775772,
+                 'average_loss': 0.36633214354515076,
                  'epoch': 1}]
 
     with unittest.mock.patch('sys.argv', ['[this gets ignored]',
